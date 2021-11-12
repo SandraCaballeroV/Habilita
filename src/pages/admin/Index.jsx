c6b0aaa606f350e92b055be326c67b4e719a1b7c
unknown <sandracaballero11@gmail.com>
2021-11-11 20:52:27 -0500

    cambios en el Index

--- src/pages/admin/Index.jsx
+++ src/pages/admin/Index.jsx
@@ -8,7 +8,8 @@ const Admin = () => {
      <div className='flex h-full w-full flex-col items-center justify-start p-8'>
       <div className='flex flex-col w-full'>
         <h2 className='text-3xl font-extrabold text-gray-900  justify-start'>
-        BIENVENIDO AL SISTEMA DE GESTION 
+        BIENVENIDO AL SISTEMA DE GESTION!   
+        SELECCIONE SU RUTA EN EL MENÚ
          </h2>
         </div>
         </div>
