44e7eb0bd93d3c53e402bd17847287883dfd9c80
unknown <sandracaballero11@gmail.com>
2021-11-11 20:49:58 -0500

    cambios nuevos

--- src/pages/admin/Index.jsx
+++ src/pages/admin/Index.jsx
@@ -4,16 +4,15 @@ import React from 'react';
 const Admin = () => {
 
   
-  return <div>
-     
-        <h2 className='text-3xl font-extrabold text-gray-900  justify-center'>
+  return (
+     <div className='flex h-full w-full flex-col items-center justify-start p-8'>
+      <div className='flex flex-col w-full'>
+        <h2 className='text-3xl font-extrabold text-gray-900  justify-start'>
         BIENVENIDO AL SISTEMA DE GESTION 
-        </h2>
-        <h2 className='text-3xl font-extrabold text-gray-900  justify-center'>
-          ESCOJA UNA OPCIÓN DEL MENÚ
-          </h2>
+         </h2>
         </div>
-        
+        </div>
+  )
 };
 
 export default Admin;
